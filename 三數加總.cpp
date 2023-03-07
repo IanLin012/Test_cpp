9aa637b0002bb2d35e76f12da95a57511ba53bed
012 <95599572+IanLin012@users.noreply.github.com>
2023-03-07 10:42:42 +0800

    Update 三數加總.cpp

--- 三數加總.cpp
+++ 三數加總.cpp
@@ -5,35 +5,26 @@ int main()
 {
     int n;
     cin >> n;
-    int arr[n], ans[3]={0};
+    int arr[n], *ptr[n];
     for(int i=0 ; i<n ; i++)
     {
         cin >> arr[i];
+        ptr[i] = &arr[i];
     }
     sort(arr, arr+n);
+    int compare[3] = {0};
     for(int a=0 ; a<n-2 ; a++)
     {
         for(int b=a+1 ; b<n-1 ; b++)
         {
             for(int c=b+1 ; c<n ; c++)
             {
-                if(arr[a]+arr[b]+arr[c] == 0)
+                if((arr[a]+arr[b]+arr[c]==0) && (arr[a]!=compare[0]&&arr[b]!=compare[1]&&arr[c]!=compare[2]))
                 {
-                    ans[0] = arr[a];
-                    ans[1] = arr[b];
-                    ans[2] = arr[c];
-                    if(ans[0]!=ans[1] && ans[1]!=ans[2])
-                    {
-                        for(int i=0 ; i<3 ; i++)
-                        {
-                            if(i==2)
-                                cout << ans[i] << endl;
-                            else
-                                cout << ans[i] << " ";
-                        }
-                    }
+                    cout << *ptr[a] << " " << *ptr[b] << " "<< *ptr[c] << endl;
+                    int compare[3] = {*ptr[a], *ptr[b], *ptr[c]};
                 }
-                else if((a == n-3) && (b==n-2) && (c==n-1) && (ans[0]==ans[1] && ans[1]==ans[2]))
+                else if(((a==n-3)&&(b==n-2)&&(c==n-1)) && (compare[0]==0&&compare[1]==0&&compare[2]==0))
                     cout << "0" << endl;
             }
         }
